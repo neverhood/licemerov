d5b50fedb8d2df8493433c80fa83fc4fff20a500
Vlad Khomich <khomich.vlad@gmail.com>
2011-01-26 22:53:30 +0200

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Licemerov::Application.config.secret_token = '56ef06738f7a607e0a63c14ada0ecdd9f5901b0f68ac1a356e4dca09455f10cf8e97c72006aeac543e81ed7ed06d28fc791547bb251f052a874380582282dcef'
