a5bcec7a59eda5279ca4f0714f72f81f56e84f0f
Vlad Khomich <khomich.vlad@gmail.com>
2011-01-27 21:09:05 +0200

    user sessions controller

---
+++ app/controllers/users/user_sessions_controller.rb
@@ -0,0 +1,29 @@
+class UserSessionsController < ApplicationController
+
+  US = UserSession # shortcut
+
+  skip_before_filter :existent_user
+  before_filter :require_no_user, :only => [:new, :create]
+  before_filter :require_user, :only => [:destroy]
+
+  def new
+    @session = US.new
+  end
+
+  #noinspection RubyArgCount
+  def create
+    @session = US.new(params[:user_session])
+    if @session.save
+      redirect_to(home_page(@session.login), :notice => 'Welcome!')
+    else
+      render :action => :new
+    end
+  end
+
+  def destroy
+    @session = US.find
+    @session.destroy
+    redirect_to login_path
+  end
+
+end
