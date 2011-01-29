69699a8bd558b8609998c18c6e0595044bda3822
Vlad Khomich <khomich.vlad@gmail.com>
2011-01-29 12:54:40 +0200

    user tests

--- test/unit/helpers/users_helper_test.rb
+++ test/unit/helpers/users_helper_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
+
+  def valid_user
+    User.new(:login => 'vlad2', :password => '12345', :password_confirmation => '12345',
+    :email => 'vlad.khomich@gmail.com')
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,46 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test 'Valid user' do
+    assert users(:vlad).save
   end
+
+  test 'Wrong password confirmation' do
+    assert !User.new(:login => 'vlad2', :password => '12345', :password_confirmation => '123456',
+    :email => 'vlad.khomich@gmail.com').save
+  end
+
+  test 'Blank password' do
+    assert !User.new(:login => 'vlad2', :password => '', :password_confirmation => '',
+    :email => 'vlad.khomich@gmail.com').save
+  end
+
+  test 'Restricted login' do
+    assert !User.new(:login => 'admin', :password => '12345', :password_confirmation => '12345',
+    :email => 'admin@licemerov.net').save
+  end
+
+  test 'Too short login' do
+    assert !User.new(:login => 'vl', :password => '12345', :password_confirmation => '12345',
+    :email => 'admin@licemerov.net').save
+  end
+
+  test 'Too long login' do
+    assert !User.new(:login => 'tahtistoolonglogin', :password => '12345', :password_confirmation => '12345',
+    :email => 'admin@licemerov.net').save
+  end
+
+  test 'Taken login' do
+    users(:vlad).save # login: vlad, email: vkhomich.vlad@gmail.com
+    assert !User.new(:login => 'vlad', :email => 'admin@licemerov.net', :password => '12345',
+    :password_confirmation => '12345').save, 'Saved user with duplicated login'
+  end
+
+  test 'Taken email' do
+    users(:vlad).save # login: vlad, email: khomich.vlad@gmail.com
+    assert !User.new(:login => 'vlad2', :email => 'khomich.vlad@gmail.com', :password => '12345',
+    :password_confirmation => '12345').save
+  end
+
 end
