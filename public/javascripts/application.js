b506869180ed943d0881806c9001e81020ad6ce4
Vlad Khomich <khomich.vlad@gmail.com>
2011-01-30 13:45:50 +0200

    main page entry form javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+    var $loader = "<img src='images/loader.gif' />";
+$(document).ready(function() {
+
+    $('#parent_form').bind("ajax:beforeSend", function() {toggleLoader(this)}).
+            bind("ajax:complete", function() {toggleLoader(this)});
+
+});
+
+function toggleLoader(form) {
+    var submit = $(this).children('input[type="submit"]');
+    if (submit.is(':visible')) {
+        submit.hide();
+        form.append($loader);
+    } else {
+        submit.show(); $loader.remove();
+    }
+}
