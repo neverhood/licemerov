123e75267bd44c7e35610d3616829b71e936b975
Vlad Khomich <khomich.vlad@gmail.com>
2011-03-12 23:29:56 +0200

    removed unneeded

--- app/models/users/user_details.rb
+++ app/models/users/user_details.rb
@@ -9,34 +9,13 @@ end
 class UserDetails < ActiveRecord::Base
 
   attr_accessible :first_name, :last_name, :birth_date, :country, :city, :avatar, 
-     :phone, :website #, :crop_x, :crop_y, :crop_w, :crop_h
+     :phone, :website
 
-#  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
-#  belongs_to :user
-#  has_attached_file :avatar,
-#                    #:path => Settings.services.assets.path,
-#                    #:url => Settings.services.assets.url,
-#                    :default_style => :thumb,
-#                    #:default_url =>
-#                    #    Settings.services.assets.defaults_path,
-#                    #:whiny_thumbnails => true,
-#                    :styles => {
-#                        :thumb => {:geometry => "110x110>", :format => :jpg, :processors => [:cropper]},
-#                        :small => {:geometry => "200x200>", :format => :jpg, :processors => [:cropper]},
-#                        :large => ['600x600>', :jpg]
-#                    }
-#  after_post_process :save_avatar_dimensions
-#
-#  validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif'],
-#                                    :unless => Proc.new  { |model| model.avatar }
-#  validates_attachment_size :avatar, :less_than => 1.megabytes, :unless => Proc.new { |model| model.avatar }
-#
-#  before_save :randomize_file_name, :if => :uploading_avatar?
-#  after_update :reprocess_avatar, :if => :cropping?
+
   
   validates :country, :predefined_country => true
   validates :city, :length => {:maximum => 25}
-#  validate :avatar_geometry
+
 
 
   def age
@@ -47,37 +26,9 @@ class UserDetails < ActiveRecord::Base
     Countries.where(:name => self.country).first
   end
 
-#  def cropping?
-#    ![crop_x, crop_y, crop_w, crop_h].find {|a| !a.is_a? Fixnum }.nil?
-#  end
+
 
   private
 
-#  def uploading_avatar?
-#    avatar_file_name_changed?
-#  end
-#
-#  def randomize_file_name
-#    extension = File.extname(avatar_file_name).downcase
-#    self.avatar.instance_write(:file_name, "#{ActiveSupport::SecureRandom.hex(16)}#{extension}")
-#  end
-#
-#  def reprocess_avatar
-#    avatar.reprocess!
-#  end
-#
-#  def save_avatar_dimensions # See lib/attachment.rb for details
-#    if avatar.geometry.width > 100 && avatar.geometry.height > 100
-#      self.avatar_dimensions = {:original => {:width => avatar.geometry.width, :height => avatar.geometry.height},
-#                                :large => {:width => avatar.geometry(:large).width, :height => avatar.geometry(:large).height}}.to_s
-#    end
-#  end
-#
-#  def avatar_geometry
-#    if self.avatar_file_name_changed?
-#      self.errors.add(:avatar,
-#              'is too small( should be at least 100x100 px image)') if (avatar.geometry.height < 100 || avatar.geometry.width < 100)
-#    end
-#  end
 
 end
