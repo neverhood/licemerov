f64501b7761bf5fdfd63cc7004f14e86165b8952
Vlad Khomich <khomich.vlad@gmail.com>
2011-03-15 02:15:01 +0000

    tokens . refactor

--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -2,20 +2,58 @@
 // if user has less then 100 friends then div#friends-json is populated with friends data which is then used for auto completion
 // if user has more then 100 friends then request is sent to server and awaits for json in response
 
+var eql = function(elem1, elem2) {
+  if (elem1.length === elem2.length && elem1.length === $(elem1).filter(elem2).length) {
+    return true
+  } else { return false };
+};
+
+function buildElem(value) {
+  return $('<span class="token">' + value +
+      '<strong class="remove-token">X </strong></span>' )
+}
 
 
 $(document).ready(function() {
+
+    $('.remove-token').live('click', function() {
+      var f = $('#message_recipient');
+      var newWidth = f.width() + $(this).parent().outerWidth(true) + 10;
+      if (newWidth > 340) newWidth = 340;
+      f.width(newWidth);
+      $(this).parent().remove();
+    });
+
     if ( $('#friends-json').length ) {
         $('#message_recipient').autocomplete({
             minLength: 1,
             source: $.licemerov.user.friends,
             focus: function(event, ui) {
-                $('#message_recipient').val(ui.item.value);
+           //     $('#message_recipient').val(ui.item.value);
                 return false;
             },
             select: function( event, ui) {
-                $('#message_recipient').val(ui.item.value);
+                //$('#message_recipient').val(ui.item.value);
                 // $('#please-avatar').val(ui.item.avatar);
+                var elem = buildElem(ui.item.value).hide();
+                var textbox = $('#message_recipient');
+                $('body').append(elem);
+                var newWidth = ( textbox.width() - elem.outerWidth(true) - 5 );
+                if (newWidth > 35) {
+                  textbox.css({width:newWidth + 'px'});
+                  textbox.before(elem.show());
+                } else {
+                  if ( elem.width() < 35 ) {
+                    textbox.before(elem.show());
+                    textbox.css({width:'335px'});
+                  } else {
+                    textbox.css({width:'335px'});
+                    newWidth = ( textbox.width() - elem.outerWidth(true) - 5);
+                    textbox.css({width:newWidth + 'px'});
+                    textbox.before(elem.show());
+                  }
+                }
+
                 return false
             }
         }).
