2e1f35976edf0e55a55a9dd0acc251deb7ddcdf2
Vlad Khomich <khomich.vlad@gmail.com>
2011-03-21 21:19:04 +0200

    messages autocompletion: existence check for ajax suggestioning

--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -3,74 +3,40 @@
 // if user has more then 100 friends then request is sent to server and awaits for json in response
 
 
-if (! Array.indexOf) {
-    Array.indexOf = [].indexOf ?
-      function(arr, obj, from) { return arr.indexOf(obj, from); } :
-      function(arr, obj, from) {
-        var length = arr.length,
-            i = from ? parseInt( (1*from) + (from < 0 ? 1:0), 10) : 0;
-        i = i < 0 ? 0 : i;
-        for (; i<1; i++) {
-          if (i in arr && arr[i] === obj ) { return i; }
-        }
-        return -1;
-      };
-}
-
-function findIndexByValue(value) {
-
-  var tokens = $('#message_recipient').autocomplete('option', 'source'),
-      iterator = tokens.length;
-
-  while(iterator--) {
-    if ( tokens[iterator].value == value ) {
-      return iterator;
-    }
-  }
-
-  return -1;
-}
-
 function buildElem(item) {
     return $('<div class="token" id="' + item.value + '"><span class="v">'
             + item.value + '</span><span class="remove-token"></span></div>')
-           .data({
-               'avatarUrl': item.avatar,
-               'value': item.value,
-               'index': item.index
-               });
+            .data({
+                      'avatarUrl': item.avatar,
+                      'value': item.value,
+                      'index': item.index
+                  });
 }
 
-function existentToken(value) {
-    var existent = false;
-    $.each($('.token'), function() {
-        if ( $(this).data('value') == value )
-            existent = true;
-    });
-    return existent;
-}
 
 function appendAvatar(avatar_url) {
     $('#avatar').html('<img src="' + avatar_url + '" />');
 }
+
+
 
 $(document).ready(function() {
 
 
 
     $('.token').live({
-       click: function() {
-           var $this = $(this);
-           if (! $this.hasClass('token-focused'))
+        click: function() {
+            var $this = $(this);
+            if (! $this.hasClass('token-focused'))
                 focusToken($this);
-       },
+        },
         mouseenter: function() {
             if ( $(this).data('avatarUrl') )
                 appendAvatar($(this).data('avatarUrl'));
         },
         mouseleave: function() {
             var tokens = $('.token'),
-                focusedToken = $('.token-focused')[0];
+                    focusedToken = $('.token-focused')[0];
             if (focusedToken) {
                 appendAvatar( $( focusedToken ).data('avatarUrl') )
             } else {
@@ -98,64 +64,114 @@ $(document).ready(function() {
         }
     });
 
+    var tempToken = $('<div class="token"></div>')
+            .appendTo('body');
+    var minWidth = parseInt($(tempToken).css('min-width'));
+    tempToken.remove();
 
-    if ( $('#friends-json').length ) {
-        var tempToken = $('<div class="token"></div>')
-                .appendTo('body');
-        var minWidth = parseInt($(tempToken).css('min-width'));
-        tempToken.remove();
+    var inputBox = $('#message_recipient'),
+            container = inputBox.parent(),
+            containerRightPos = function() {
+                return (container.offset().left + container.width());
+            },
+            origWidth = inputBox.width(),
+            margin = inputBox
+                    .clone(true)
+                    .hide()
+                    .appendTo('body').outerWidth() - origWidth,
+            calcOffset = function() {
+                var items = container.children('.token'),
+                        lastItem = $(items[items.length - 1]),
+                        lastItemRightPos = lastItem[0]?
+                                (lastItem.offset().left + lastItem.width()) : (inputBox.offset().left + margin);
+                return ( containerRightPos() - lastItemRightPos  );
+            },
+            removeToken = function(token) {
+                var currentSource = $('#message_recipient').autocomplete('option', 'source');
 
-        var inputBox = $('#message_recipient'),
-                container = inputBox.parent(),
-                containerRightPos = function() {
-                    return (container.offset().left + container.width());
-                },
-                origWidth = inputBox.width(),
-                margin = inputBox
-                        .clone(true)
-                        .hide()
-                        .appendTo('body').outerWidth() - origWidth,
-                calcOffset = function() {
-                    var items = container.children('.token'),
-                            lastItem = $(items[items.length - 1]),
-                            lastItemRightPos = lastItem[0]?
-                                    (lastItem.offset().left + lastItem.width()) : (inputBox.offset().left + margin);
-                    return ( containerRightPos() - lastItemRightPos  );
-                },
-                removeToken = function(token) {
-                    var currentSource = $('#message_recipient').autocomplete('option', 'source');
+                currentSource.push( {
+                    'avatar': token.data('avatarUrl'),
+                    'value': token.data('value')
+                });
 
-                    currentSource.push( {
-                        'avatar': token.data('avatarUrl'),
-                        'value': token.data('value')
-                                          });
+                $('#message_recipient').autocomplete('option', 'source', currentSource);
 
-                    $('#message_recipient').autocomplete('option', 'source', currentSource);
+                token.remove();
 
-                    token.remove();
+                var tokens = $('.token');
 
-                    var tokens = $('.token');
+                if (tokens.length == 0) {
+                    appendAvatar('/avatars/thumb/missing.png');
+                }
+                else
+                    appendAvatar($(tokens[0]).data('avatarUrl'));
 
-                    if (tokens.length == 0) {
-                       appendAvatar('/avatars/thumb/missing.png');
-                    }                                      
-                    else
-                        appendAvatar($(tokens[0]).data('avatarUrl'));
+                var currentOffset = calcOffset();
+                if ( currentOffset < minWidth ) {
+                    inputBox.width(origWidth);
+                } else {
+                    inputBox.width(currentOffset - margin);
+                }
+            },
+            findIndexByValue = function(value) {
+                var tokens = $('#message_recipient').autocomplete('option', 'source'),
+                        iterator = tokens.length;
 
-                    //token.remove();
-                    var currentOffset = calcOffset();
-                    if ( currentOffset < minWidth ) {
-                        inputBox.width(origWidth);
-                    } else {
-                        inputBox.width(currentOffset - margin);
+                while(iterator--) {
+                    if ( tokens[iterator].value == value ) {
+                        return iterator;
                     }
-                },
-                focusToken = function(token) {
-                    $('.token-focused').removeClass('token-focused');
-                    appendAvatar(token.data('avatarUrl'));
-                    token.addClass('token-focused');
-                };
+                }
 
+                return -1;
+            },
+            focusToken = function(token) {
+                $('.token-focused').removeClass('token-focused');
+                appendAvatar(token.data('avatarUrl'));
+                token.addClass('token-focused');
+            };
+
+    var autocompleteOnSelect = function( event, ui) {
+        var tokens = $('.token');
+
+        $('.token-focused').removeClass('token-focused');
+
+        var currentSource = $('#message_recipient').autocomplete('option', 'source'),
+                i = findIndexByValue(ui.item.value);
+        currentSource.splice(i, 1);
+        $('#message_recipient').autocomplete('option', 'source', currentSource);
+
+        if (  tokens.length >= 15 )
+            return false;
+
+
+        var elem = buildElem(ui.item)
+                .hide()
+                .appendTo('body'),
+                elemWidth = elem.outerWidth(true),
+                currentOffset = calcOffset();
+
+        tokens.push(elem[0]);
+
+        appendAvatar( $(tokens[0]).data('avatarUrl') );
+
+        if ((currentOffset) >= elemWidth) {
+            if ( (currentOffset - elemWidth) > minWidth ) {
+                inputBox.before( elem.show() )
+                        .width( currentOffset - elemWidth - margin  );
+            } else {
+                inputBox.before( elem.show() )
+                        .width( origWidth );
+            }
+        } else {
+            inputBox.before( elem.show() )
+                    .width( calcOffset() - margin );
+        }
+        inputBox.val('');
+        return false
+    };
+
+    if ($.licemerov.user.friends.length < 0) {
         inputBox.autocomplete({
             minLength: 1,
             source: $.licemerov.user.friends,
@@ -164,51 +180,13 @@ $(document).ready(function() {
                 return false;
             },
 
-            select: function( event, ui) {
-                var tokens = $('.token');
-
-                $('.token-focused').removeClass('token-focused');
-
-                var currentSource = $('#message_recipient').autocomplete('option', 'source'),
-                    i = findIndexByValue(ui.item.value);
-                currentSource.splice(i, 1);
-                $('#message_recipient').autocomplete('option', 'source', currentSource);
-
-                if (  tokens.length == 15 )
-                   return false;
-
-
-                var elem = buildElem(ui.item)
-                        .hide()
-                        .appendTo('body'),
-                        elemWidth = elem.outerWidth(true),
-                        currentOffset = calcOffset();
-
-                tokens.push(elem[0]);
-
-                appendAvatar( $(tokens[0]).data('avatarUrl') );
-
-                if ((currentOffset) >= elemWidth) {
-                    if ( (currentOffset - elemWidth) > minWidth ) {
-                        inputBox.before( elem.show() )
-                                .width( currentOffset - elemWidth - margin  );
-                    } else {
-                        inputBox.before( elem.show() )
-                                .width( origWidth );
-                    }
-                } else {
-                    inputBox.before( elem.show() )
-                            .width( calcOffset() - margin );
-                }
-                inputBox.val('');
-                return false
-            }
+            select: autocompleteOnSelect
         }).
                 data('autocomplete')._renderItem = function( ul, item ) {
 
-                return $('<li></li>').data('item.autocomplete', item).
-                        append('<a>' + item.value + '</a>').
-                        appendTo(ul);
+            return $('<li></li>').data('item.autocomplete', item).
+                    append('<a>' + item.value + '</a>').
+                    appendTo(ul);
 
         };
     } else {
@@ -228,7 +206,177 @@ $(document).ready(function() {
                         response( data );
                     }
                 });
-            }
+            },
+            select: autocompleteOnSelect
         });
     }
+
+//} else {
+//    var cache = {},
+//            lastXhr;
+//    $( "#message_recipient" ).autocomplete({
+//        minLength: 2,
+//        source: function( request, response ) {
+//            var term = request.term;
+//            if ( term in cache ) {
+//                response( cache[term] );
+//                return;
+//            }
+//            lastXhr = $.getJSON( "/messages/new", request, function( data, status, xhr ) {
+//                cache[ term ] = data;
+//                if ( xhr === lastXhr ) {
+//                    response( data );
+//                }
+//            });
+//        }
+//    });
+//    }
+
+
+
+//    if ( $('#friends-json').length ) {
+//        var tempToken = $('<div class="token"></div>')
+//                .appendTo('body');
+//        var minWidth = parseInt($(tempToken).css('min-width'));
+//        tempToken.remove();
+//
+//        var inputBox = $('#message_recipient'),
+//                container = inputBox.parent(),
+//                containerRightPos = function() {
+//                    return (container.offset().left + container.width());
+//                },
+//                origWidth = inputBox.width(),
+//                margin = inputBox
+//                        .clone(true)
+//                        .hide()
+//                        .appendTo('body').outerWidth() - origWidth,
+//                calcOffset = function() {
+//                    var items = container.children('.token'),
+//                            lastItem = $(items[items.length - 1]),
+//                            lastItemRightPos = lastItem[0]?
+//                                    (lastItem.offset().left + lastItem.width()) : (inputBox.offset().left + margin);
+//                    return ( containerRightPos() - lastItemRightPos  );
+//                },
+//                removeToken = function(token) {
+//                    var currentSource = $('#message_recipient').autocomplete('option', 'source');
+//
+//                    currentSource.push( {
+//                        'avatar': token.data('avatarUrl'),
+//                        'value': token.data('value')
+//                    });
+//
+//                    $('#message_recipient').autocomplete('option', 'source', currentSource);
+//
+//                    token.remove();
+//
+//                    var tokens = $('.token');
+//
+//                    if (tokens.length == 0) {
+//                        appendAvatar('/avatars/thumb/missing.png');
+//                    }
+//                    else
+//                        appendAvatar($(tokens[0]).data('avatarUrl'));
+//
+//                    var currentOffset = calcOffset();
+//                    if ( currentOffset < minWidth ) {
+//                        inputBox.width(origWidth);
+//                    } else {
+//                        inputBox.width(currentOffset - margin);
+//                    }
+//                },
+//                findIndexByValue = function(value) {
+//                    var tokens = $('#message_recipient').autocomplete('option', 'source'),
+//                            iterator = tokens.length;
+//
+//                    while(iterator--) {
+//                        if ( tokens[iterator].value == value ) {
+//                            return iterator;
+//                        }
+//                    }
+//
+//                    return -1;
+//                },
+//                focusToken = function(token) {
+//                    $('.token-focused').removeClass('token-focused');
+//                    appendAvatar(token.data('avatarUrl'));
+//                    token.addClass('token-focused');
+//                };
+//
+//        var autocompleteOnSelect = function( event, ui) {
+//            var tokens = $('.token');
+//
+//            $('.token-focused').removeClass('token-focused');
+//
+//            var currentSource = $('#message_recipient').autocomplete('option', 'source'),
+//                    i = findIndexByValue(ui.item.value);
+//            currentSource.splice(i, 1);
+//            $('#message_recipient').autocomplete('option', 'source', currentSource);
+//
+//            if (  tokens.length >= 15 )
+//                return false;
+//
+//
+//            var elem = buildElem(ui.item)
+//                    .hide()
+//                    .appendTo('body'),
+//                    elemWidth = elem.outerWidth(true),
+//                    currentOffset = calcOffset();
+//
+//            tokens.push(elem[0]);
+//
+//            appendAvatar( $(tokens[0]).data('avatarUrl') );
+//
+//            if ((currentOffset) >= elemWidth) {
+//                if ( (currentOffset - elemWidth) > minWidth ) {
+//                    inputBox.before( elem.show() )
+//                            .width( currentOffset - elemWidth - margin  );
+//                } else {
+//                    inputBox.before( elem.show() )
+//                            .width( origWidth );
+//                }
+//            } else {
+//                inputBox.before( elem.show() )
+//                        .width( calcOffset() - margin );
+//            }
+//            inputBox.val('');
+//            return false
+//        };
+//
+//        inputBox.autocomplete({
+//            minLength: 1,
+//            source: $.licemerov.user.friends,
+//            focus: function(event, ui) {
+//                appendAvatar(ui.item.avatar);
+//                return false;
+//            },
+//
+//            select: autocompleteOnSelect
+//        }).
+//                data('autocomplete')._renderItem = function( ul, item ) {
+//
+//            return $('<li></li>').data('item.autocomplete', item).
+//                    append('<a>' + item.value + '</a>').
+//                    appendTo(ul);
+//
+//        };
+//    } else {
+//        var cache = {},
+//                lastXhr;
+//        $( "#message_recipient" ).autocomplete({
+//            minLength: 2,
+//            source: function( request, response ) {
+//                var term = request.term;
+//                if ( term in cache ) {
+//                    response( cache[term] );
+//                    return;
+//                }
+//                lastXhr = $.getJSON( "/messages/new", request, function( data, status, xhr ) {
+//                    cache[ term ] = data;
+//                    if ( xhr === lastXhr ) {
+//                        response( data );
+//                    }
+//                });
+//            }
+//        });
+//    }
 });
